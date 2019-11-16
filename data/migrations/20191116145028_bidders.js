e33e341e64948554768ba0941f1c995023ca22b0
Jason Loomis <jason@jrloomis.com>
2019-11-16 14:59:11 -0500

    bidder & seller migrations

---
+++ data/migrations/20191116145028_bidders.js
@@ -0,0 +1,24 @@
+module.exports.up = function(knex) {
+  return knex.schema.createTable("bidders", tbl => {
+    tbl.increments();
+    tbl.string("firstName").notNullable();
+    tbl.string("lastName").notNullable();
+    tbl
+      .string("email ")
+      .notNullable()
+      .unique();
+    tbl.string("streetAddress").notNullable();
+    tbl.string("city").notNullable();
+    tbl.string("state").notNullable();
+    tbl.string("zipCode").notNullable();
+    tbl
+      .string("username")
+      .notNullable()
+      .unique();
+    tbl.string("password").notNullable();
+  });
+};
+
+module.exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("bidders");
+};
---
+++ data/migrations/20191116145036_sellers.js
@@ -0,0 +1,25 @@
+module.exports.up = function(knex) {
+  return knex.schema.createTable("sellers", tbl => {
+    tbl.increments();
+    tbl.string("firstName").notNullable();
+    tbl.string("lastName").notNullable();
+    tbl.string("organization");
+    tbl
+      .string("email ")
+      .notNullable()
+      .unique();
+    tbl.string("streetAddress").notNullable();
+    tbl.string("city").notNullable();
+    tbl.string("state").notNullable();
+    tbl.string("zipCode").notNullable();
+    tbl
+      .string("username")
+      .notNullable()
+      .unique();
+    tbl.string("password").notNullable();
+  });
+};
+
+module.exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("sellers");
+};
