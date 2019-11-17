62f771b28ce192fb6cbd98039310783b76e87c18
Jason Loomis <jason@jrloomis.com>
2019-11-16 20:57:38 -0500

    setup testing env

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,6 +2,8 @@ const knex = require("knex");
 
 const knexConfig = require("../knexfile");
 
+// module.exports = knex(knexConfig.development);
+
 const environment = process.env.DB_ENV || "development";
 
 module.exports = knex(knexConfig[environment]);
--- knexfile.js
+++ knexfile.js
@@ -20,6 +20,20 @@ module.exports = {
     }
   },
 
+  testing: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./data/test.db3"
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
+
   production: {
     client: "postgresql",
     connection: {
