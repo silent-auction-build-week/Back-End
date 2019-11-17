32cd7bbd4011fc9f0c24c37e1d580736ae5337b3
Jason Loomis <jason@jrloomis.com>
2019-11-16 20:56:18 -0500

    import auth routes

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const middleware = require("./middleware/global");
 // todo import routes
+const authRouter = require("./auth/auth-router");
 
 const server = express();
 
 middleware(server);
 
 // todo server.use routes
+server.use("/auth", authRouter);
 
 //* sanity
 server.get("/", (req, res) => res.json({ everyone: "good news there is" }));
