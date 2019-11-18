7969994e7bc28437b17a6ea577ccaff5497ed9c6
Jason Loomis <jason@jrloomis.com>
2019-11-18 13:38:20 -0500

    mock items added to db, db reseeded

---
+++ data/migrations/20191118124909_items.js
@@ -0,0 +1,21 @@
+module.exports.up = function(knex) {
+  return knex.schema.createTable("items", tbl => {
+    tbl.increments();
+    tbl.string("item_name", 100).notNullable();
+    tbl.string("description", 500).notNullable();
+    tbl.binary("img_url");
+    tbl.float("price", 8, 2);
+    tbl
+      .integer("seller_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("sellers")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+  });
+};
+
+module.exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("items");
+};
