5a510c9bf443aed45228b920243a306bd269597f
Jason Loomis <jason@jrloomis.com>
2019-11-19 14:24:32 -0500

    import auctions routes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const middleware = require("./middleware/global");
 // * import routes
 const authRouter = require("./auth/auth-router");
 const itemsRouter = require("./routes/items");
+const auctionsRouter = require("./routes/auctions");
 
 const server = express();
 
@@ -13,6 +14,7 @@ middleware(server);
 // * use routes
 server.use("/auth", authRouter);
 server.use("/api", itemsRouter);
+server.use("/api", auctionsRouter);
 
 // * sanity
 server.get("/", (req, res) =>
