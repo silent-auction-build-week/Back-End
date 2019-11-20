cb1d2864817499a07ea28d9ade8558334f578903
Jason Loomis <jason@jrloomis.com>
2019-11-20 12:27:03 -0500

    changed public to middleware in expres.static()

--- api/server.js
+++ api/server.js
@@ -1,12 +1,12 @@
-const cors = require('cors');
-const helmet = require('helmet');
-const express = require('express');
+const cors = require("cors");
+const helmet = require("helmet");
+const express = require("express");
 
-const help = require('../api/help.js');
-const authRoute = require('../auth/auth-router');
-const auctRoute = require('./auctions/auctRoute.js');
-const bidRoute = require('./bidding/bidRoute.js');
-const userRoute = require('./user/userRoute.js');
+const help = require("../api/help.js");
+const authRoute = require("../auth/auth-router");
+const auctRoute = require("./auctions/auctRoute.js");
+const bidRoute = require("./bidding/bidRoute.js");
+const userRoute = require("./user/userRoute.js");
 
 const server = express();
 
@@ -14,11 +14,11 @@ server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
-server.use(express.static('public'));
+server.use(express.static("middleware"));
 server.use("/api/auth", authRoute);
 server.use("/api/auction", help.verifyToken, auctRoute);
 server.use("/api/bids", help.verifyToken, bidRoute);
 server.use("/api/user", help.verifyToken, userRoute);
-server.get("/", (req,res) => {
+server.get("/", (req, res) => {
   res.send("Thanks for bidding with us!");
-})
+});
