31bd5cf9bfc851e2a4c2456b9d3d0c3d7033982d
Jason Loomis <jason@jrloomis.com>
2019-11-20 14:01:21 -0500

    import bidder and seller routes

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const middleware = require("./middleware/global");
 const authRouter = require("./auth/auth-router");
 const itemsRouter = require("./routes/items");
 const auctionsRouter = require("./routes/auctions");
+const biddersRouter = require("./routes/bidders");
+const sellersRouter = require("./routes/sellers");
 
 const server = express();
 
@@ -15,6 +17,8 @@ middleware(server);
 server.use("/auth", authRouter);
 server.use("/api", itemsRouter);
 server.use("/api", auctionsRouter);
+server.use("/api", biddersRouter);
+server.use("/api", sellersRouter);
 
 // * sanity
 server.get("/", (req, res) =>
